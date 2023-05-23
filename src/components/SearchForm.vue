ff7fe8070e974f7bfd36f4acd0821f1b0f07b837
lanceJiang <lancejiangwei@qq.com>
2023-05-23 15:41:29 +0800

    feat: SearchForm 组件变更(变更为setup)

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,9 +1,9 @@
-<script lang="jsx">
-
+<script lang="tsx">
 const emits = ['update:searchParams']
 import {
-	defineComponent
-	// PropType
+	defineComponent,
+	watch,
+	ref
 } from 'vue'
 const props = {
 	forms: {
@@ -27,201 +27,211 @@ const props = {
 export const SearchForm = defineComponent({
 	name: 'SearchForm',
 	emits,
-	render() {
-		const _this = this
-		const { forms, searchParams, formConfig = {}, searchHandler } = this
-		let warpClass = 'search-form-container'
-		const itemRender = form => {
-			const { prop, itemType, itemWidth, options, change, itemStyle = '', ...formOthers } = form
-			const _options = options || []
-
-			let disabled = form.disabled
-			if (disabled === undefined) {
-				disabled = false
-			}
-			// 优化后的 change事件
-			const formatterChange = async () => {
-				if (change) {
-					return change(searchParams, _options)
-				}
-			}
-			switch (itemType) {
-				// 下拉框
-				case 'select':
-					return (
-						<el-select
-							{...formOthers}
-							v-model={searchParams[prop]}
-							onChange={formatterChange}
-							style={`${itemStyle} width:${itemWidth || '160px'};`}
-							disabled={disabled}
-							clearable={form.clearable !== false}
-							placeholder={form.placeholder || `请选择${form.label}`}
-						>
-							{_options.map((option, optionIndex) => {
-								const value = typeof option === 'object' ? option[form.valueKey || 'value'] : option
-								const label = typeof option === 'object' ? option[form.labelKey || 'label'] : option
-								return <el-option key={value} label={label} value={value} />
-							})}
-						</el-select>
-					)
-				// 单选框
-				case 'radio':
-					return (
-						<el-radio-group
-							v-model={searchParams[prop]}
-							{...formOthers}
-							disabled={disabled}
-							onChange={formatterChange}
-							style={`${itemStyle} width:${itemWidth || 'auto'};`}
-						>
-							{_options.map((option, optionIndex) => {
-								const value = typeof option === 'object' ? option[form.valueKey || 'value'] : option
-								const label = typeof option === 'object' ? option[form.labelKey || 'label'] : option
-								return (
-									<el-radio key={optionIndex + '_local'} label={value}>
-										{label}
-									</el-radio>
-								)
-							})}
-						</el-radio-group>
-					)
-				// 级联
-				case 'cascader':
-					return (
-						<el-cascader
-							{...formOthers}
-							v-model={searchParams[prop]}
-							onChange={formatterChange}
-							style={`${itemStyle} ${itemWidth && 'width: ' + itemWidth};`}
-							disabled={disabled}
-							clearable={form.clearable ?? true}
-							filterable={form.filterable ?? true}
-							options={_options}
-							placeholder={form.placeholder || `请选择${form.label}`}
-						/>
-					)
-				// 数字
-				case 'inputNumber':
-					return (
-						<el-input-number
-							class="rate100"
-							{...formOthers}
-							v-model={searchParams[prop]}
-							onChange={formatterChange}
-							style={`${itemStyle} width:${itemWidth || '130px'};`}
-							disabled={disabled}
-							placeholder={form.placeholder || `${form.label || ''}`}
-							precision={form.precision || 0}
-						/>
-					)
-
-				// 日期选择
-				case 'date':
-					return (
-						<el-date-picker
-							{...formOthers}
-							v-model={searchParams[prop]}
-							onChange={formatterChange}
-							style={`${itemStyle} width:${itemWidth || '160px'};`}
-							disabled={disabled}
-							placeholder={form.placeholder || `请选择${form.label}`}
-							value-format={form.valueFormat || 'YYYY-MM-DD'}
-						/>
-					)
-				// 日期区间
-				case 'dateRange':
-					return (
-						<el-date-picker
-							type="daterange"
-							{...formOthers}
-							v-model={searchParams[prop]}
-							onChange={formatterChange}
-							startPlaceholder={form.startPlaceholder ?? '开始日期'}
-							endPlaceholder={form.startPlaceholder ?? '结束日期'}
-							style={`${itemStyle} width:${itemWidth || '200px'};`}
-							disabled={disabled}
-							value-format={form.valueFormat || 'YYYY-MM-DD'}
-						/>
-					)
-				// switch
-				case 'switch':
-					return <el-switch {...formOthers} v-model={searchParams[prop]} onChange={formatterChange} disabled={disabled} />
-				case 'input':
-				default:
-					return (
-						<el-input
-							{...formOthers}
-							v-model={searchParams[prop]}
-							onChange={formatterChange}
-							disabled={disabled}
-							placeholder={form.placeholder || `请输入${form.label}`}
-							style={`${itemStyle} width:${itemWidth || '160px'};`}
-						/>
-					)
-			}
-		}
-		return (
-			<div class={warpClass}>
-				<div class="search-form-flex">
-					<el-form ref="baseForm" inline={true} size="default" class="search-form-flex-wrap" model={searchParams} {...formConfig}>
-						{forms.map((form, idx) => {
-							// 通过 form.visible 控制 是否展示
-							return (
-								<el-form-item v-show={form.visible !== false} key={idx} {...form}>
-									{itemRender(form)}
-								</el-form-item>
-							)
-						})}
-					</el-form>
-					<div class="action-wrap">
-						<el-button size="default" plain onClick={this.local_resetHandler}>
-							<el-icon>
-								<Refresh />
-							</el-icon>
-							重置
-						</el-button>
-						<el-button size="default" type="primary" onClick={this.searchHandler}>
-							<el-icon>
-								<Search />
-							</el-icon>
-							查询
-						</el-button>
-					</div>
-				</div>
-			</div>
-		)
-	},
 	props,
-	data() {
-		return {}
-	},
-	watch: {
-		searchParams: {
-			handler(data, old) {
-				if (!this.initSearchParams) {
-					this.initSearchParams = { ...data }
+	setup(props, ctx) {
+		const formRef = ref(/*formRef*/)
+		let initSearchParams = undefined
+		watch(
+			() => props.searchParams,
+			(newValue, oldValue) => {
+				if (!initSearchParams && newValue) {
+					initSearchParams = { ...newValue }
 				}
 			},
-			immediate: true
-		}
-	},
-	created() {},
-	methods: {
-		// 重置
-		local_resetHandler() {
-			// 若有reset 将不会触发默认重置的操作
-			const emitReset = this.reset
-			if (emitReset) {
-				emitReset({ ...this.initSearchParams })
-			} else {
-				// this.$refs.baseForm.resetFields()
-				// 撤回为初始化状态
-				this.$emit('update:searchParams', { ...this.initSearchParams })
+			{
+				immediate: true
 			}
-		},
-		searchHandler() {
-			this.$emit('update:searchParams', { ...this.searchParams })
+		)
+
+		// 重置搜索
+		const local_resetHandler = () => {
+			// 若有reset 将不会触发默认重置的操作
+			const emitReset = props.reset
+			const _initSearchParams = { ...(initSearchParams || {}) }
+			if (emitReset) {
+				emitReset(_initSearchParams)
+			} else {
+				// formRef.value!.resetFields()
+				// 撤回为初始化状态
+				ctx.emit('update:searchParams', _initSearchParams)
+			}
+		}
+		// 搜索
+		const searchHandler = () => {
+			ctx.emit('update:searchParams', { ...props.searchParams })
+		}
+		// 强行修改初始化数据(用于 重置方法进行数据重置)
+		const forceUpdateInitParams = (searchParams = props.searchParams) => {
+			initSearchParams = { ...searchParams }
+		}
+		ctx.expose({
+			formRef,
+			forceUpdateInitParams
+		})
+		// render渲染
+		return () => {
+			const { forms, searchParams, formConfig = {} } = props
+			let warpClass = 'search-form-container'
+			const itemRender = form => {
+				const { prop, itemType, itemWidth, options, change, itemStyle = '', ...formOthers } = form
+				const _options = options || []
+
+				let disabled = form.disabled
+				if (disabled === undefined) {
+					disabled = false
+				}
+				// 优化后的 change事件
+				const formatterChange = async () => {
+					if (typeof change === 'function') {
+						return change(searchParams, _options)
+					}
+				}
+				switch (itemType) {
+					// 下拉框
+					case 'select':
+						return (
+							<el-select
+								{...formOthers}
+								v-model={searchParams[prop]}
+								onChange={formatterChange}
+								style={`${itemStyle} width:${itemWidth || '160px'};`}
+								disabled={disabled}
+								clearable={form.clearable ?? true}
+								placeholder={form.placeholder || `请选择${form.label}`}
+							>
+								{_options.map((option, optionIndex) => {
+									const value = typeof option === 'object' ? option[form.valueKey || 'value'] : option
+									const label = typeof option === 'object' ? option[form.labelKey || 'label'] : option
+									return <el-option key={value} label={label} value={value} />
+								})}
+							</el-select>
+						)
+					// 单选框
+					case 'radio':
+						return (
+							<el-radio-group
+								v-model={searchParams[prop]}
+								{...formOthers}
+								disabled={disabled}
+								onChange={formatterChange}
+								style={`${itemStyle} width:${itemWidth || 'auto'};`}
+							>
+								{_options.map((option, optionIndex) => {
+									const value = typeof option === 'object' ? option[form.valueKey || 'value'] : option
+									const label = typeof option === 'object' ? option[form.labelKey || 'label'] : option
+									return (
+										<el-radio key={optionIndex + '_local'} label={value}>
+											{label}
+										</el-radio>
+									)
+								})}
+							</el-radio-group>
+						)
+					// 级联
+					case 'cascader':
+						return (
+							<el-cascader
+								{...formOthers}
+								v-model={searchParams[prop]}
+								onChange={formatterChange}
+								style={`${itemStyle} ${itemWidth && 'width: ' + itemWidth};`}
+								disabled={disabled}
+								clearable={form.clearable ?? true}
+								filterable={form.filterable ?? true}
+								options={_options}
+								placeholder={form.placeholder || `请选择${form.label}`}
+							/>
+						)
+					// 数字
+					case 'inputNumber':
+						return (
+							<el-input-number
+								class="rate100"
+								{...formOthers}
+								v-model={searchParams[prop]}
+								onChange={formatterChange}
+								style={`${itemStyle} width:${itemWidth || '130px'};`}
+								disabled={disabled}
+								placeholder={form.placeholder || `${form.label || ''}`}
+								precision={form.precision || 0}
+							/>
+						)
+
+					// 日期选择
+					case 'date':
+						return (
+							<el-date-picker
+								{...formOthers}
+								v-model={searchParams[prop]}
+								onChange={formatterChange}
+								style={`${itemStyle} width:${itemWidth || '160px'};`}
+								disabled={disabled}
+								placeholder={form.placeholder || `请选择${form.label}`}
+								value-format={form.valueFormat || 'YYYY-MM-DD'}
+							/>
+						)
+					// 日期区间
+					case 'dateRange':
+						return (
+							<el-date-picker
+								type="daterange"
+								{...formOthers}
+								v-model={searchParams[prop]}
+								onChange={formatterChange}
+								startPlaceholder={form.startPlaceholder ?? '开始日期'}
+								endPlaceholder={form.startPlaceholder ?? '结束日期'}
+								style={`${itemStyle} width:${itemWidth || '200px'};`}
+								disabled={disabled}
+								value-format={form.valueFormat || 'YYYY-MM-DD'}
+							/>
+						)
+					// switch
+					case 'switch':
+						return <el-switch {...formOthers} v-model={searchParams[prop]} onChange={formatterChange} disabled={disabled} />
+					case 'input':
+					default:
+						return (
+							<el-input
+								{...formOthers}
+								v-model={searchParams[prop]}
+								onChange={formatterChange}
+								disabled={disabled}
+								placeholder={form.placeholder || `请输入${form.label}`}
+								style={`${itemStyle} width:${itemWidth || '160px'};`}
+							/>
+						)
+				}
+			}
+			return (
+				<div class={warpClass}>
+					<div class="search-form-flex">
+						<el-form ref={formRef} inline={true} size="default" class="search-form-flex-wrap" model={searchParams} {...formConfig}>
+							{forms.map((form, idx) => {
+								// 通过 form.visible 控制 是否展示
+								return (
+									<el-form-item v-show={form.visible !== false} key={idx} {...form}>
+										{itemRender(form)}
+									</el-form-item>
+								)
+							})}
+						</el-form>
+						<div class="action-wrap">
+							<el-button size="default" plain onClick={local_resetHandler}>
+								<el-icon>
+									<Refresh />
+								</el-icon>
+								重置
+							</el-button>
+							<el-button size="default" type="primary" onClick={searchHandler}>
+								<el-icon>
+									<Search />
+								</el-icon>
+								查询
+							</el-button>
+						</div>
+					</div>
+				</div>
+			)
 		}
 	}
 })
