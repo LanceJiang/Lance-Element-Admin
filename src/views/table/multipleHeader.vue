92218d53876ba511b9495ca4c15e0808a076b9d9
lanceJiang <lancejiangwei@qq.com>
2023-06-05 14:51:08 +0800

    feat: 多级表头示例 更新 列变更排序 （修复 ELTable 列排序变更 table 更新不同步问题）

--- src/views/table/multipleHeader.vue
+++ src/views/table/multipleHeader.vue
@@ -1,16 +1,18 @@
 <template>
 	<div class="flex-column-page-wrap pageWrap">
 		<LeTable
+			ref="localTable"
 			v-model:searchParams="searchParams"
 			class="local_table tabs_content-wrap"
 			:list="localList"
 			:total="total"
 			:options="options"
 			:columns="localColumns"
+			v-model:checkedOptions="tabs_checkedColumns"
+			:columnsConfig="tabs_columnsConfig"
 		>
 			<template #toolLeft>
 				<h3 style="line-height: 36px;">多级表头示例</h3>
-				<el-button @click="toggleColumns">切换 columns</el-button>
 			</template>
 			<template #validNum="{row, column}">
 				<span style="color: #e6a23c;">{{row[column.property]}}</span><br/>
@@ -21,6 +23,7 @@
 </template>
 <script setup lang="tsx" name="multipleHeader">
 import { ref, toRefs, reactive, computed, watch, onMounted } from 'vue'
+import { cloneDeep } from 'lodash-es';
 import i18n from '@/lang/index'
 import { ElMessage } from 'element-plus'
 import { get_multipleHeaderList } from '@/views/table/queryApi.js'
@@ -40,7 +43,7 @@ const searchParams = ref({
 
 const showAll = ref(true)
 
-const localColumns = computed(() => {
+/*const localColumns = computed(() => {
 	const columns = [
 		{
 			prop: 'areaName',
@@ -76,10 +79,10 @@ const localColumns = computed(() => {
 			minWidth: '500px',
 			slots: {
 				// default 会被覆盖
-				/*default: ({row, column, $index }) => {
+				/!*default: ({row, column, $index }) => {
 					console.error(row, 'row column $index', column, $index)
 					return '其中...'
-				},*/
+				},*!/
 				header: ({ column, $index }) => {
 					// console.error('column $index', column, $index)
 					return <el-button onClick={testClick}>其中...</el-button>
@@ -116,11 +119,164 @@ const localColumns = computed(() => {
 			]
 		}
 	]
-})
-// 切换columns
-const toggleColumns = () => {
-	showAll.value = !showAll.value
+})*/
+const columns = [
+	{
+		prop: 'projectName',
+		label: '项目',
+		minWidth: '200px'
+	},
+	{
+		prop: 'areaName',
+		label: '所在区域',
+		minWidth: '300px'
+	},
+	{
+		prop: 'caseTotal',
+		label: '上报问题数',
+		minWidth: '100px',
+		fixed: 'right',
+		formatter (row, column, cellValue, index) {
+			return <div>
+				<el-link style="color: #409eff;">{cellValue}</el-link><br/>
+				(formatter)
+			</div>
+		}
+	},
+	{
+		label: '其中',
+		prop: 'in',
+		align: 'center',
+		// align: 'right',
+		fixed: 'right',
+		minWidth: '500px',
+		slots: {
+			// default 会被覆盖
+			/*default: ({row, column, $index }) => {
+				console.error(row, 'row column $index', column, $index)
+				return '其中...'
+			},*/
+			header: ({ column, $index }) => {
+				// console.error('column $index', column, $index)
+				return <el-button onClick={testClick}>其中...</el-button>
+			}
+		},
+		children: [
+			{
+				prop: 'invalidNum',
+				label: '作废数（宗）',
+				minWidth: 200,
+				sortable: true,
+				slots: {
+					default: (args) => {
+						// console.log(args)
+						const {row, column, $index} = args
+						// console.error($index, '$index')
+						return <div>
+							<span style="color: red;">{row[column.property]}</span><br/>
+							(slots.default: fn)
+						</div>
+					}
+				}
+			},
+
+			{
+				prop: 'validNum',
+				label: '有效问题数（宗）',
+				sortable: true,
+				minWidth: 300,
+				slots: {
+					default: 'validNum'
+				}
+			}
+		]
+	}
+]
+// 列配置对象
+const tabs_columnsConfig = {
+	// 所有的 columns 配置
+	columns,
+	// 默认展示配置
+	defaultCheckedOptions: [columns[0]]
 }
+const tabs_checkedColumns = ref([columns[0]])
+const localColumns = computed(() => {
+	const checkedColumns = tabs_checkedColumns.value as any[]
+	if (!checkedColumns.length) return columns
+	const clone_columns = cloneDeep(columns)
+	const sortColumnChildren = (localColumn, targetColumn) => {
+		const cur_children = localColumn.children
+		if(Array.isArray(cur_children) && Array.isArray(targetColumn.children)) {
+			// console.error(JSON.stringify(cur_children), 'cur_children   targetColumn_children', JSON.stringify(targetColumn.children))
+			// children 排序
+			localColumn.children = targetColumn.children.map(_column => {
+				const findColumn = cur_children.find(l_column => l_column.prop === _column.prop)
+				if(findColumn) {
+					if(Array.isArray(findColumn.children)) {
+						return sortColumnChildren(findColumn, _column)
+					}
+					return findColumn
+				}
+				return false
+			}).filter(Boolean)
+		}
+		return localColumn
+	}
+	return checkedColumns.map(v => {
+		// 深度克隆
+		const cur = clone_columns.find(column => column.prop === v.prop)
+		if (cur) {
+			// children 内嵌处理
+			return sortColumnChildren(cur, v)
+		}
+		return false
+	})
+	.filter(Boolean)
+})
+const localTable = ref(/*localTable*/)
+watch(() => localColumns.value, () => {
+	const ELTable = localTable.value?.tableRef
+	console.log(ELTable, 'ELTable')
+	/*if(ELTable) {
+		console.error('测试')
+		/!*
+		const table_states = ELTable.store.states
+		const lastColumns = table_states._columns.value
+		console.error(lastColumns, ' lastColumns')
+		const sortColumnChildren = (localColumn, targetColumn) => {
+			const cur_children = localColumn.children
+			if(Array.isArray(cur_children) && Array.isArray(targetColumn.children)) {
+				// console.error(JSON.stringify(cur_children), 'cur_children   targetColumn_children', JSON.stringify(targetColumn.children))
+				// children 排序
+				localColumn.children = targetColumn.children.map(_column => {
+					const findColumn = cur_children.find(l_column => l_column.property === _column.prop)
+					if(findColumn) {
+						if(Array.isArray(findColumn.children)) {
+							return sortColumnChildren(findColumn, _column)
+						}
+						return findColumn
+					}
+					return false
+				}).filter(Boolean)
+			}
+			return localColumn
+		}
+		const newColumns = localColumns.value.map(v => {
+			// 深度克隆
+			const cur = lastColumns.find(column => column.property === v.prop)
+			if (cur) {
+				// children 内嵌处理
+				return sortColumnChildren(cur, v)
+			}
+			return false
+		}).filter(Boolean)
+		// const newColumns = localColumns.map(v => lastColumns.find(_v => _v.property === v.prop)).filter(Boolean)
+		table_states._columns.value = newColumns
+		ELTable.store.updateColumns()
+		// ELTable.doLayout?.()*!/
+	}*/
+})
+
 const testClick = (e) => {
 	// console.error('click 测试', e)
 	ElMessage.warning('this is a testClick.')
