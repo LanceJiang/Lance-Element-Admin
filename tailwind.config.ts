514a4275a851470e972f2e979fb4e65b79b6e625
LanceJiang <lancejiangwei@qq.com>
2024-06-12 09:53:52 +0800

    feat: tailwindcss 支持

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+export default {
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+	/*// 这部分 css 需要通过比较复杂的计算得出，所以要用 `safelist` 保证它们出现在最终 css 里
+	safelist: [
+		{
+			pattern: /w-\d+\/\d+/
+		},
+		{
+			pattern: /grid-cols-[5-9]/
+		}
+	]*/
+} satisfies Config
