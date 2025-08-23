0aa4cfef7535d5360229ec0f47086d0472526209
lanceJiang <lancejiangwei@qq.com>
2025-08-23 16:38:28 +0800

    feat: Icon 替换为 SvgIcon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,18 @@
+<template>
+	<svg class="le-icon" aria-hidden="true">
+		<use :xlink:href="`#${iconClass}`" :fill="color" />
+	</svg>
+</template>
+
+<script lang="ts" setup name="LeIcon">
+const props = defineProps({
+	iconClass: {
+		type: String,
+		require: true
+	},
+	color: {
+		type: String,
+		default: ''
+	}
+})
+</script>
