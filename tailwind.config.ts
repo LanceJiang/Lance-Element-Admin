42f60575010e0e20e2990563e775b8acfcf6e7f8
lanceJiang <lancejiangwei@qq.com>
2025-09-04 20:50:51 +0800

    fix: tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,18 +24,42 @@ export default {
 			'2xl': '1536px'
 			// => @media (min-width: 1536px) { ... }
 		},
-		colors: {
-			/*primary: 'var(--el-color-primary)',*/
-			error: '#f56c6c', // 'var(--el-color-error)',
-			warning: '#e6a23c', // 'var(--el-color-warning)',
-			success: '#67c23a', // 'var(--el-color-success)',
-			blue: '#1fb6ff'
-			// pink: '#ff49db',
-			// orange: '#ff7849',
-			// green: '#13ce66',
-			// 'gray-dark': '#273444',
-			// gray: '#8492a6',
-			// 'gray-light': '#d3dce6'
+		colors: ({ colors }) => {
+			return {
+				...colors,
+				/*inherit: colors.inherit,
+				current: colors.current,
+				transparent: colors.transparent,
+				black: colors.black,
+				white: colors.white,
+				slate: colors.slate,
+				gray: colors.gray,
+				zinc: colors.zinc,
+				neutral: colors.neutral,
+				stone: colors.stone,
+				red: colors.red,
+				orange: colors.orange,
+				amber: colors.amber,
+				yellow: colors.yellow,
+				lime: colors.lime,
+				green: colors.green,
+				emerald: colors.emerald,
+				teal: colors.teal,
+				cyan: colors.cyan,
+				sky: colors.sky,
+				blue: colors.blue,
+				indigo: colors.indigo,
+				violet: colors.violet,
+				purple: colors.purple,
+				fuchsia: colors.fuchsia,
+				pink: colors.pink,
+				rose: colors.rose*/
+				// /*primary: 'var(--el-color-primary)',*/
+				error: '#f56c6c', // 'var(--el-color-error)',
+				warning: '#e6a23c', // 'var(--el-color-warning)',
+				success: '#67c23a', // 'var(--el-color-success)',
+				blue: '#1fb6ff'
+			}
 		},
 		extend: {}
 	},
